4ab9523f9c3453f40c461a8ac9ff58e446e9fd8e
zime songbian <zimesongbian@gmail.com>
2026-06-22 09:57:59 +0000

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:botbenin/integrations/supabase_service.dart';
+import 'package:nowa_runtime/nowa_runtime.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const WaouhApp());
+@NowaGenerated()
+late final SharedPreferences sharedPrefs;
+
+@NowaGenerated()
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPrefs = await SharedPreferences.getInstance();
+  await SupabaseService().initialize();
+  runApp(const MyApp());
 }
 
-class WaouhApp extends StatelessWidget {
-  const WaouhApp({super.key});
+@NowaGenerated({'visibleInNowa': false})
+class MyApp extends StatelessWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WAOUH',
-      home: const Scaffold(
-        body: Center(
-          child: Text('WAOUH Flutter'),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppState>(create: (context) => AppState()),
+      ],
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppState.of(context).theme,
+        initialRoute: 'HomePage',
+        routes: {'HomePage': (context) => HomePage()},
       ),
     );
   }
