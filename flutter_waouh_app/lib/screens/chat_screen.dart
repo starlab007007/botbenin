a641a2afa22399f595cfd25b013e96a15d111873
zimesongbian007 <zimesongbian007@gmail.com>
2026-06-25 17:30:17 +0100

    feat: add native chat screen

---
+++ flutter_waouh_app/lib/screens/chat_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
