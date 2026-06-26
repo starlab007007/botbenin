fe991d60d7f383e5b9232697b037edf7d1130c4a
zimesongbian007 <zimesongbian007@gmail.com>
2026-06-26 09:52:27 +0100

    feat: add live UI module

---
+++ flutter_waouh_app/lib/live/live_widgets.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LiveWidgetsMarker extends StatelessWidget {
+  const LiveWidgetsMarker({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
