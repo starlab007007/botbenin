a5d6244d5ac1aeac1cbb8d51114158904c4cf722
zimesongbian007 <zimesongbian007@gmail.com>
2026-06-26 09:58:00 +0100

    feat: include resolved match screen

--- flutter_waouh_app/lib/live/live_screens.dart
+++ flutter_waouh_app/lib/live/live_screens.dart
@@ -1,4 +1,5 @@
 export 'live_chat_screens.dart';
 export 'live_inbox_screen.dart';
 export 'live_notifications_screen.dart';
+export 'live_resolved_match_screen.dart';
 export 'live_status_screen.dart';
