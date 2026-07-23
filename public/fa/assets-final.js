67ede36ad326bee44de79bbf295c7dd6077577a0
zimesongbian007 <zimesongbian007@gmail.com>
2026-07-23 09:07:43 +0100

    feat(fa): finaliser les assets visuels FA IA V5.2

---
+++ public/fa/assets-final.js
@@ -0,0 +1,4 @@
+window.FA_ASSETS.fa_chain_mystic_hero="data:image/webp;base64,"+window.FA_ASSET_CHUNKS.hero.join("");
+window.FA_ASSETS.fa_chain_reference="data:image/webp;base64,"+window.FA_ASSET_CHUNKS.chain.join("");
+window.FA_ASSETS.cowrie_closed_real="data:image/webp;base64,"+window.FA_ASSET_CHUNKS.closed.join("");
+window.FA_ASSETS.cowrie_open_real="data:image/webp;base64,"+window.FA_ASSET_CHUNKS.open.join("");
