4d36871a3168922495010c7cc6bdf3ae93bc3ce2
zimesongbian007 <zimesongbian007@gmail.com>
2026-07-23 18:32:03 +0100

    feat(fa): forcer les assets réels Flutter

---
+++ public/fa/fa-real-assets.js
@@ -0,0 +1,6 @@
+(() => {
+  'use strict';
+  window.FA_ASSETS = window.FA_ASSETS || {};
+  window.FA_ASSETS.cowrie_open_real = './assets/cowrie_open_flutter_clean.png?v=20260723-fa-flutter-v6';
+  window.FA_ASSETS.cowrie_closed_real = './assets/cowrie_closed_flutter_clean.png?v=20260723-fa-flutter-v6';
+})();
