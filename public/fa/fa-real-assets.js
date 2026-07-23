b6ebe1d52c113bf7d4aff466f7374ef5bbb97fb2
zimesongbian007 <zimesongbian007@gmail.com>
2026-07-23 18:50:57 +0100

    fix(fa): forcer les vrais cauris Flutter

--- public/fa/fa-real-assets.js
+++ public/fa/fa-real-assets.js
@@ -1,6 +1,29 @@
 (() => {
   'use strict';
+
   window.FA_ASSETS = window.FA_ASSETS || {};
-  window.FA_ASSETS.cowrie_open_real = './assets/cowrie_open_flutter_clean.png?v=20260723-fa-flutter-v6';
-  window.FA_ASSETS.cowrie_closed_real = './assets/cowrie_closed_flutter_clean.png?v=20260723-fa-flutter-v6';
+  window.FA_ASSET_CHUNKS = window.FA_ASSET_CHUNKS || {
+    open: [],
+    closed: []
+  };
+
+  const openChunks = window.FA_ASSET_CHUNKS.open || [];
+  const closedChunks = window.FA_ASSET_CHUNKS.closed || [];
+
+  if (openChunks.length) {
+    window.FA_ASSETS.cowrie_open_real =
+      'data:image/webp;base64,' + openChunks.join('');
+  }
+
+  if (closedChunks.length) {
+    window.FA_ASSETS.cowrie_closed_real =
+      'data:image/webp;base64,' + closedChunks.join('');
+  }
+
+  window.FA_ASSETS.cowrie_open_flutter_clean =
+    window.FA_ASSETS.cowrie_open_real;
+  window.FA_ASSETS.cowrie_closed_flutter_clean =
+    window.FA_ASSETS.cowrie_closed_real;
+
+  window.FA_REAL_COWRIES_VERSION = '20260723-fa-flutter-v8';
 })();
