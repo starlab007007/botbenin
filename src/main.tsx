ce4315b9bb6c3fe9b5a4e7dc1551d9808f6d127a
zimesongbian007 <zimesongbian007@gmail.com>
2026-08-10 21:55:11 +0100

    feat(chat): charger module PrivatAI dans app chat

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './privatai-chat-promo';
 
 createRoot(document.getElementById("root")!).render(<App />);
